f0386cc496ac4b6f13a42235254f37a549813297
LZ <2739542493@qq.com>
2023-02-15 10:32:23 +0800

    首页，路由

---
+++ src/views/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 顶部通栏 -->
+  <nav>顶部通栏</nav>
+  <!-- 头部组件 -->
+  <head>
+    头部组件
+  </head>
+  <!-- 内容容器 -->
+  <div>
+    <!-- 二级路由 -->
+    <router-view />
+  </div>
+  <!-- 底部组件 -->
+  <footer>底部组件</footer>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped lang="less"></style>
