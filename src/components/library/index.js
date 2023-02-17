63f1721747db391cbd964c2ea2bb7facc416740a
LZ <2739542493@qq.com>
2023-02-17 21:10:18 +0800

    面包屑组件和批量注册组件完成

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -3,20 +3,38 @@
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
 
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import HomePanel from './xtx-panel.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import HomePanel from './xtx-panel.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxBread from './xtx-bread.vue'
 import defaultImg from '@/assets/images/200.png'
 
+// 导入library文件夹下的所有组件
+// 批量导入需要使用一个函数 require.context(dir,deep,matching)
+// 参数：1. 目录  2. 是否加载子目录  3. 加载的正则匹配
+const importFn = require.context('./', false, /\.vue$/)
+// console.dir(importFn.keys()) 文件名称数组
 export default {
   install(app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
-    app.component('XtxSkeleton', XtxSkeleton) // 骨架
-    app.component('XtxCarousel', XtxCarousel) // 轮播图
-    app.component('XtxMore', XtxMore) // 查看更多
-    app.component('HomePanel', HomePanel) // 主页分类面板
+    // app.component('XtxSkeleton', XtxSkeleton) // 骨架
+    // app.component('XtxCarousel', XtxCarousel) // 轮播图
+    // app.component('XtxMore', XtxMore) // 查看更多
+    // app.component('HomePanel', HomePanel) // 主页分类面板
+    // app.component('XtxBreadItem', XtxBreadItem) // 面包屑
+    // app.component('XtxBread', XtxBread) // 面包屑
+
+    // 批量注册全局组件
+    importFn.keys().forEach((key) => {
+      // 导入组件
+      const component = importFn(key).default
+      // 注册组件
+      app.component(component.name, component)
+    })
+
     defineDirective(app) // 自定义指令
   }
 }
